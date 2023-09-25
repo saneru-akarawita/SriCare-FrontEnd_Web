614ef67c1ecc0e541bbf2d063b9a7de0f61fd693
Saneru Akarawita <saneru.akarawita@gmail.com>
2023-09-26 00:29:08 +0530

    bill section - view bills complete

---
+++ src/components/currentbill.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Card, CardContent, Typography, Button } from '@mui/material';
+import styled from 'styled-components';
+
+
+
+const CurrentBill = () => {
+
+  return (
+    <div></div>
+  );
+};
+
+export default CurrentBill;
