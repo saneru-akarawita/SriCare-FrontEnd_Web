1884c578f57817e8bb2b7160609769e51dec2c8e
Dewmini-Vithanage <2020cs194@stu.ucsc.cmb.ac.lk>
2023-09-29 18:26:51 +0530

    VAS

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import App from '../pages/samplePage';
+import Vas from '../pages/valueAddedServices';
 
 
 export const mainRouter = createBrowserRouter([
@@ -8,6 +9,10 @@ export const mainRouter = createBrowserRouter([
     path: '/',
     element: <App />,
   },
+  {
+    path: '/valueAddedServices',
+    element: <Vas />,
+  }
 ]);
 
 export default mainRouter;
