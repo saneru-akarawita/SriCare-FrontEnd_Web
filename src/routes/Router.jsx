800e50bcbc968287a1b502e6ccb6f9a050a8c1d2
PamithWelikala123 <108875335+PamithWelikala123@users.noreply.github.com>
2023-09-29 22:27:59 +0530

    done
    new commit

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -4,6 +4,9 @@ import App from '../pages/samplePage';
 import MainBill from '../pages/mainBillPage';
 import ChatRoom from '../pages/chatRoom';
 import Vas from '../pages/valueAddedServices';
+import Service from '../pages/Services';
+import Amount from '../pages/Amount';
+import Done from '../pages/Done';
 
 
 export const mainRouter = createBrowserRouter([
@@ -23,7 +26,21 @@ export const mainRouter = createBrowserRouter([
   {
     path: '/chat',
     element: <ChatRoom />
-  }
+  },
+  {
+    path: '/service',
+    element: <Service />
+  } 
+  ,{
+    path: '/amount',
+    element: <Amount />
+  },
+  {
+    path: '/done',
+    element: <Done />
+  },
+
+
 ]);
 
 export default mainRouter;
