9e27b57087f5633f9a877ae3b40cc16da6ff3d67
Saneru Akarawita <85979369+saneru-akarawita@users.noreply.github.com>
2023-09-29 21:33:41 +0530

    Revert "VAS"

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,12 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import App from '../pages/samplePage';
-import Vas from '../pages/valueAddedServices';
 import MainBill from '../pages/mainBillPage';
 import ChatRoom from '../pages/chatRoom';
 import Services from '../pages/Services';
 import Amount from '../pages/Amount';
 import Done from '../pages/Done';
+
 
 export const mainRouter = createBrowserRouter([
   {
@@ -35,11 +35,7 @@ export const mainRouter = createBrowserRouter([
     path: '/done',
     element: <Done />
   },
-  {
-    path: '/valueAddedServices',
-    element: <Vas />,
-  },
-
+  
 ]);
 
 export default mainRouter;
