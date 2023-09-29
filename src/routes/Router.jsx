d4cfbb1597755ffc3db8f5481da37038b7b7c7c6
Dewmini-Vithanage <2020cs194@stu.ucsc.cmb.ac.lk>
2023-09-29 22:03:41 +0530

    added route

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import App from '../pages/samplePage';
 import MainBill from '../pages/mainBillPage';
 import ChatRoom from '../pages/chatRoom';
+import Vas from '../pages/valueAddedServices';
 
 
 export const mainRouter = createBrowserRouter([
@@ -10,7 +11,10 @@ export const mainRouter = createBrowserRouter([
     path: '/',
     element: <App />,
   },
-
+  {
+    path: '/valueAddedServices',
+    element: <Vas />,
+  },
   {
     path: '/allbills',
     element: <MainBill />
